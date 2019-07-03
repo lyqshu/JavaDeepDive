f890a063a65ace39ed805be96cb973217d13ff84
lyqshu <lyqshu@163.com>
2019-07-03 15:28:20 +0800

    first commit

---
+++ src/main/java/ArrayListTest.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by i030518 on 2019/7/3.
+ */
+public class ArrayListTest {
+    public static void main(String[] args) {
+        List<String> ls = new ArrayList<String>();
+        ls.add("hello");
+    }
+}
