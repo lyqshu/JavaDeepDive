23ed23d4f481fd891334b7d8518a50b19d72bda6
lyqshu <lyqshu@163.com>
2019-09-30 13:30:39 +0800

    polish

---
+++ src/main/java/yq/java/deepdive/Base.java
@@ -0,0 +1,14 @@
+package yq.java.deepdive;
+
+/**
+ * Created by i030518 on 2019/9/21.
+ */
+public class Base {
+    public void process(){
+        System.out.println("Process");
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/main/java/yq/java/deepdive/YQDeepDive.java
@@ -0,0 +1,8 @@
+package yq.java.deepdive;
+
+/**
+ * Created by i030518 on 2019/9/21.
+ */
+public class YQDeepDive {
+
+}
